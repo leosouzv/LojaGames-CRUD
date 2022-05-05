4b956dedeeb12ecd6f12bae06bfc38da316c82e6
leosouzv <leogrdss@icloud.com>
2022-05-05 13:38:29 -0300

    Implemented the relationship between tables

--- src/main/java/com/generation/gamesxd/model/Categoria.java
+++ src/main/java/com/generation/gamesxd/model/Categoria.java
@@ -1,10 +1,16 @@
 package com.generation.gamesxd.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="tb_categoria")
@@ -15,4 +21,25 @@ public class Categoria {
 	public Long id;
 	
 	public String nome;
+	
+	@OneToMany(mappedBy="categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 }
--- src/main/java/com/generation/gamesxd/model/Produto.java
+++ src/main/java/com/generation/gamesxd/model/Produto.java
@@ -4,9 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name= "tb_produto")
@@ -23,4 +26,41 @@ public class Produto {
 	
 	@NotNull
 	public int valor;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipo_midia() {
+		return tipo_midia;
+	}
+
+	public void setTipo_midia(String tipo_midia) {
+		this.tipo_midia = tipo_midia;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
 }
